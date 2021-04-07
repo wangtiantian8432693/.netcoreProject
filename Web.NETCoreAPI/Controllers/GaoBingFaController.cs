2d89f9972990a3b6ee682d28da699f08a36268ee
wangtiantian <w@DESKTOP-G848G42>
2021-04-07 17:03:59 +0800

    gaobingfa

---
+++ Web.NETCoreAPI/Controllers/GaoBingFaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.NETCoreAPI.Controllers
+{
+    /// <summary>
+    /// 高并发处理
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GaoBingFaController : ControllerBase
+    {
+
+    }
+}
